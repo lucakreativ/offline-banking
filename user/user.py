8004f810b34c177bb65db1fcb42b04063d6518dd
ich <net.a@gmx.de>
2023-07-21 12:36:32 +0200

    create and load the usr keys

--- user/user.py
+++ user/user.py
@@ -2,9 +2,9 @@ from Crypto.PublicKey import ECC
 
 def create_user_key():
     priv = ECC.generate(curve='ed25519')
-    with open('usr-key/private.pem', 'wt') as f:
+    with open('private.pem', 'wt') as f:
         f.write(priv.export_key(format='PEM'))
 
     opn = priv.public_key()
-    with open('usr-key/open.pem', 'wt') as f:
+    with open('open.pem', 'wt') as f:
         f.write(opn.export_key(format='PEM'))
