4e18176969a485c0c661c162a412f3d01008e5e0
ich <net.a@gmx.de>
2023-07-21 12:46:12 +0200

    func to sign usr keys

--- bank/bank.py
+++ bank/bank.py
@@ -1,5 +1,6 @@
 from Crypto.PublicKey import ECC
-
+from Crypto.Hash import SHA512
+from Crypto.Signature import eddsa
 
 def create_keys():
     priv = ECC.generate(curve='ed25519')
@@ -10,12 +11,27 @@ def create_keys():
     with open('bank-open.pem', 'wt') as f:
         f.write(opn.export_key(format='PEM'))
 
-
 def load_public_key():
     with open('bank-open.pem', 'r') as f:
-        ECC.import_key(f.read())
-
+        return ECC.import_key(f.read())
 
 def load_private_key():
     with open('bank-private.pem', 'r') as f:
-        ECC.import_key(f.read())
+        return ECC.import_key(f.read())
+
+def load_user_key(dir):
+    with open(dir, 'r') as f:
+        return ECC.import_key(f.read())
+
+def sign_user_key(dir):
+    priv = load_private_key()
+    publ = load_public_key()
+    usr = load_user_key(dir)
+    usr_hash = SHA512.new(usr.export_key())
+    signer = eddsa.new(priv, 'rfc8032')
+    signed = signer.sign(usr_hash)
+    return signed, publ
+
+
+
+sign_user_key('../user/open.pem')
