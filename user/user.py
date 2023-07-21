42ab0d453fead6083ccb3113effff2d4a77959e6
ich <net.a@gmx.de>
2023-07-21 13:06:08 +0200

    added method to verificate the others' public keys

--- user/user.py
+++ user/user.py
@@ -1,4 +1,6 @@
 from Crypto.PublicKey import ECC
+from Crypto.Hash import SHA512
+from Crypto.Signature import eddsa
 
 def create_user_key():
     priv = ECC.generate(curve='ed25519')
@@ -8,3 +10,17 @@ def create_user_key():
     opn = priv.public_key()
     with open('open.pem', 'wt') as f:
         f.write(opn.export_key(format='PEM'))
+
+def check_publ_key(signed_key, bank_path, usr_path):
+    with open(bank_path, 'r') as f:
+        bank_key = ECC.import_key(f.read())
+    with open(usr_path, 'r') as f:
+        usr_key = ECC.import_key(f.read())
+    usr_hash = SHA512.new(usr_key.export_key())
+    verifier = eddsa.new(bank_key, 'rfc8032')
+    try:
+        verifier.verify(usr_hash, signed_key)
+        print("The key is authentic")
+    except ValueError:
+        print("The key is not authentic")
+
