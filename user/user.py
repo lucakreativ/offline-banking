40213664ec439a3cd9f91d28dba4f1b4c75af19c
ich <net.a@gmx.de>
2023-07-21 12:21:08 +0200

    moved code into bank.py and user.py

--- user/user.py
+++ user/user.py
@@ -1,14 +1,5 @@
 from Crypto.PublicKey import ECC
 
-def create_bank_key():
-    priv = ECC.generate(curve='ed25519')
-    with open('bank-key/bank-private.pem', 'wt') as f:
-        f.write(priv.export_key(format='PEM'))
-
-    opn = priv.public_key()
-    with open('bank-key/bank-open.pem', 'wt') as f:
-        f.write(opn.export_key(format='PEM'))
-
 def create_user_key():
     priv = ECC.generate(curve='ed25519')
     with open('usr-key/private.pem', 'wt') as f:
